42b4542735fe7ff96ebc6a5ee1854a65197c0dce
arthur752 <arthurjhonasdasilva@gmail.com>
2026-07-22 02:14:37 -0300

    Implementa API de produtos com conexão ao banco

--- Back-End/src/controllers/productController.js
+++ Back-End/src/controllers/productController.js
@@ -1,18 +1,115 @@
 const db = require('../config/database');
 
+
+// LISTAR PRODUTOS
 exports.getProducts = async (req, res) => {
     try {
         const [products] = await db.query(
             'SELECT * FROM products ORDER BY id DESC'
         );
 
-        res.status(200).json(products);
+        res.json(products);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Erro ao buscar produtos.'
+        });
+    }
+};
+
+
+// CRIAR PRODUTO
+exports.createProduct = async (req, res) => {
+    try {
+        const { name, price, stock, minStock } = req.body;
+
+        const [result] = await db.query(
+        `INSERT INTO products 
+        (name, price, stock, min_stock)
+        VALUES (?, ?, ?, ?)`,
+        [name, price, stock, minStock]
+);
+
+
+        const [newProduct] = await db.query(
+            'SELECT * FROM products WHERE id = ?',
+            [result.insertId]
+        );
+
+
+        res.status(201).json(newProduct[0]);
 
     } catch (error) {
         console.error(error);
 
         res.status(500).json({
-            message: 'Erro ao buscar produtos.'
+            message: 'Erro ao criar produto.'
         });
+    }
+};
+
+
+// ATUALIZAR PRODUTO
+exports.updateProduct = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const { name, price, stock, minStock } = req.body;
+
+
+        await db.query(
+            `UPDATE products 
+             SET name=?, price=?, stock=?, minStock=?
+             WHERE id=?`,
+            [name, price, stock, minStock, id]
+        );
+
+
+        const [updated] = await db.query(
+            'SELECT * FROM products WHERE id=?',
+            [id]
+        );
+
+
+        res.json(updated[0]);
+
+    } catch(error){
+
+        console.error(error);
+
+        res.status(500).json({
+            message:'Erro ao atualizar produto.'
+        });
+    }
+};
+
+
+// DELETAR PRODUTO
+exports.deleteProduct = async (req,res)=>{
+    try{
+
+        const { id } = req.params;
+
+
+        await db.query(
+            'DELETE FROM products WHERE id=?',
+            [id]
+        );
+
+
+        res.json({
+            message:'Produto removido.'
+        });
+
+
+    }catch(error){
+
+        console.error(error);
+
+        res.status(500).json({
+            message:'Erro ao remover produto.'
+        });
+
     }
 };
